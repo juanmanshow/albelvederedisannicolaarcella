603a5f06ca02f4d7dde563ae10e3818f279cea72
null <null>
2026-05-10 15:54:41 +0200

    v8.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css';
-import type { Metadata, Viewport } from 'next';
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const viewport: Viewport = {
+export const viewport = {
   themeColor: '#173746',
 };
 
